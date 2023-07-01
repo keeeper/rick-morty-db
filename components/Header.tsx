a421da17675037b8907e1c6cbed6509318cc13b1
keeeper <keeper96@gmail.com>
2023-07-01 16:42:13 +0200

    logo and favicon upd,

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,8 +6,9 @@ const Header = () => {
   return (
     <header className="mb-14 p-4 bg-header sticky top-0 z-10">
       <div className="flex justify-between items-center mx-auto max-w-5xl">
-        <Link href="/">
-          <Image src="/images/logo.svg" alt="Rick and Morty logo" width={180} height={55} priority={true} />
+        <Link href="/" className="flex items-center">
+          <Image style={{fill: '#80C60C'}} src="/images/logo.svg" alt="Rick and Morty logo" width={40} height={40} priority={true} />
+          <div className="text-sm text-white ml-2"><span className="hidden sm:block">Rick and Morty Potral</span></div>
         </Link>
         <nav className="text-sm">
           <ul className="flex gap-8">
